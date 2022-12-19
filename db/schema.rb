b39f6761b96dc740cf0d7ad14606ddeabc242c98
Sousa1909 <aluno21874@ipt.pt>
2022-12-19 14:16:31 +0000

    Documents scaffold added

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_12_212426) do
+  create_table "documents", force: :cascade do |t|
+    t.string "title"
+    t.string "fileName"
+    t.string "savePath"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
