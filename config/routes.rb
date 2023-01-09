e496e4655afe4286056e746d8107e0f516a53dac
RafaR01 <80701627+RafaR01@users.noreply.github.com>
2023-01-09 18:09:06 +0000

    Create rubyonrails CI/CD (#15)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -5,7 +7,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root "front_page#index"
+  root 'front_page#index'
 
-  get "/front_page", to: "front_page#index"
+  get '/front_page', to: 'front_page#index'
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,12 +1,110 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
 
-  test "if model can be save with null atributtes" do
+  test 'if model can be save with null atributtes' do
     c = Category.new
     assert !c.save
+  end
+
+  test 'if model can be save with a name that contains numbers' do
+    c = Category.new
+    c.name = 'Relatório'
+    if c.name.count('0-9').positive?
+      assert !c.save
+    else
+      assert c.save
+    end
+  end
+
+  test 'if model can be save with a name that contains spaces' do
+    c = Category.new
+    c.name = 'Relatório'
+    (0..c.name.length).each do |j|
+      if c.name[j] == ' '
+        assert !c.save
+        break
+      else
+        assert c.save
+      end
+    end
+  end
+
+  test 'if model can be save with a name that contains special characters' do
+    c = Category.new
+    c.name = 'Relatório'
+    if c.name.scan(%r{[!@#$%^&*()_+{}\[\]:;'"/\\?><.,]}).empty?
+      assert c.save
+    else
+      assert !c.save
+    end
+  end
+
+  test 'if model can be save with a name bigger than 15 characters' do
+    c = Category.new
+    c.name = 'Relatório'
+    if c.name.count('a-zA-Z') > 15
+      assert !c.save
+    else
+      assert c.save
+    end
+  end
+
+  test 'if model can be save with a name smaller than 5 characters' do
+    c = Category.new
+    c.name = 'Relatório'
+    if c.name.count('a-zA-Z') < 5
+      assert !c.save
+    else
+      assert c.save
+    end
+  end
+
+  test 'if model can be save with a name that is a repeatable character' do
+    c = Category.new
+    c.name = 'Relatório'
+    ('a'..'z').each do |letter|
+      if c.name.count(letter) == c.name.length
+        assert !c.save
+        break
+      else
+        assert c.save
+      end
+    end
+
+    ('A'..'Z').each do |letter|
+      if c.name.count(letter) == c.name.length
+        assert !c.save
+        break
+      else
+        assert c.save
+      end
+    end
+  end
+
+  test 'if model can be save with a name that contains uppercase letters besides the first character' do
+    c = Category.new
+    c.name = 'Relatório'
+    ('A'..'Z').each do |letter|
+      (1..c.name.length).each do |j|
+        assert !c.save if c.name[j] == letter
+      end
+    end
+    assert !c.save if c.name.match(/[ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖÙÚÛÜÝ]/)
+    assert c.save
+  end
+
+  test 'if model can be save with a name that starts with a lowercase character' do
+    c = Category.new
+    c.name = 'relatório'
+    ('a'..'z').each do |letter|
+      assert !c.save if c.name[0] == letter
+    end
+    assert c.save
   end
 end
